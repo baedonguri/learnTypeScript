eb823ae8e888bff26e90ef9190e6f18de53a2103
baedonguri <rmsdbraos21@naver.com>
2022-10-19 14:44:35 +0900

    migration ts

--- practice/designpattern/factory/concept_of_factory.ts
+++ practice/designpattern/factory/concept_of_factory.ts
@@ -1,22 +1,22 @@
 // createImage factory는 전혀 필요없는 것처럼 보임
-function createImage(name) {
-  return new Image(name);
+function createImage(width: number, height: number) {
+  return new Image(width, height);
 }
 
 // 직접 new 연산자를 사용해 Image 클래스를 인스턴스화 하는 방법도 존재
 // 하지만 new를 사용하면 하나의 특정한 유형의 객체 만을 코드에 바인딩 할 수 있음.
-const image = new Image(name);
+const image = new Image(500, 500);
 
 // 팩토리를 사용할 경우, 더 많은 유연성을 제공.
 // 다음과 같이 Image Class를 각각의 이미지 형식에 맞는 더 작은 객체들로 나누어 리팩토링.
 
-function createImageFactory(name) {
+function createImageFactory(name: string, width: number, height: number) {
   if (name.match(/\.jpeg$/)) {
-    return new JpegImage(name);
+    return new JpegImage(name, width, height);
   } else if (name.match(/\.gif$/)) {
-    return new GifImage(name);
+    return new GifImage(name, width, height);
   } else if (name.match(/\.png/)) {
-    return new PngImage(name);
+    return new PngImage(name, width, height);
   } else {
     throw new Execption("Unsupported Format");
   }
--- practice/optional_chaining.ts
+++ practice/optional_chaining.ts
@@ -1,20 +1,22 @@
-const adventurer = {
-  name: "Alice",
-  cat: {
-    name: "Dinah",
-  },
-};
-
-const dogName = adventurer.dog?.name;
-console.log(dogName);
-// expected output : undefined
-
-console.log(adventurer.someNonExistentMethod?.());
-// expected output : undefined
-
 /**
  * Optional Chaining 연산자 (?.)
  * 만약 참조가 nullish (null or undefind)이라면, 에러가 발생하는 것 대신에 표현식의 리턴 값은 undefined로 단락된다.
  * 함수 호출에서 사용될 때, 만약 주어진 함수가 존재하지 않는다면 undefined를 리턴.
  * optional chaining은 선언되지 않은 루트 객체에 사용할 수 없지만, 정의되지 않은 루트 객체와는 함께 사용할 수 있다.
  */
+
+interface CompanyInfo {
+  name: string | null | undefined;
+}
+
+let obj: CompanyInfo = {
+  name: "Facebook",
+};
+
+console.log(obj.name?.length); // 8
+
+let obj_null: CompanyInfo = {
+  name: null,
+};
+
+console.log(obj_null.name?.length); // undefined
